e300c6d8595c3ad0c00e372d05fe3b5619475e4e
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2014-08-22 09:40:06 -0300

    First commit, project creation

---
+++ Networking/Networking/MLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLAppDelegate.h
+//  Networking
+//
+//  Created by Mauricio Minestrelli on 8/22/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
